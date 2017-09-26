319f42a30a616dd2def1337776eec40a4c0d10df
Stefan Wolf <wolf@gradle.com>
2017-09-26 22:08:36 +0200

    Fix endless loop when used with Ant builder
    [FIXES JENKINS-46051]

--- src/main/resources/hudson/plugins/gradle/GradleTaskNote/script.js
+++ src/main/resources/hudson/plugins/gradle/GradleTaskNote/script.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     // created on demand
     var outline = null;
     var loading = false;
@@ -6,19 +6,21 @@
     var queue = []; // gradle tasks are queued up until we load outline.
 
     function loadOutline() {
-        if (outline != null)  return false;   // already loaded
+        if (outline != null) return false;   // already loaded
 
         if (!loading) {
             loading = true;
             var u = new Ajax.Updater(document.getElementById("side-panel"),
-                    rootURL + "/descriptor/hudson.plugins.gradle.GradleTaskNote/outline",
-            {insertion: Insertion.Bottom, onComplete: function() {
-                if (!u.success())   return; // we can't us onSuccess because that kicks in before onComplete
-                outline = document.getElementById("console-outline-body")
-                			.getElementsByTagName('ul')[0];
-                loading = false;
-                queue.each(handle);
-            }});
+                rootURL + "/descriptor/hudson.plugins.gradle.GradleTaskNote/outline",
+                {
+                    insertion: Insertion.Bottom, onComplete: function () {
+                    if (!u.success()) return; // we can't us onSuccess because that kicks in before onComplete
+                    outline = document.getElementById("gradle-console-outline-body")
+                        .getElementsByTagName('ul')[0];
+                    loading = false;
+                    queue.each(handle);
+                }
+                });
         }
         return true;
     }
@@ -42,7 +44,7 @@
 
     Behaviour.register({
         // insert <a name="..."> for each Gradle task and put it into the outline
-        "b.gradle-task" : function(e) {
+        "b.gradle-task": function (e) {
             handle(e);
         }
     });
