94788dbfe6b9ff25d9077dc231c63a658b00f549
Alexey Venderov <avenderov@gradle.com>
2022-08-29 20:01:12 +0200

    Improve workspace resolution

--- acceptance-tests/src/main/java/hudson/plugin/gradle/ath/config/WorkspaceModule.java
+++ acceptance-tests/src/main/java/hudson/plugin/gradle/ath/config/WorkspaceModule.java
@@ -5,6 +5,8 @@ import com.cloudbees.sdk.extensibility.ExtensionModule;
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
 
+import java.io.File;
+
 /**
  * @see org.jenkinsci.test.acceptance.FallbackConfig#getWorkspace
  */
@@ -13,6 +15,8 @@ public class WorkspaceModule extends AbstractModule implements ExtensionModule {
 
     @Override
     protected void configure() {
-        bind(String.class).annotatedWith(Names.named("WORKSPACE")).toInstance("build");
+        bind(String.class)
+            .annotatedWith(Names.named("WORKSPACE"))
+            .toInstance(new File(System.getProperty("user.dir"), "build").getPath());
     }
 }
