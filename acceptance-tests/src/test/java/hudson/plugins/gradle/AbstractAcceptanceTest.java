e9d635dec804028e1ff16924fd99609bceb64520
Alexey Venderov <avenderov@gradle.com>
2022-08-31 23:14:25 +0200

    Add test for skipping gradle injection

--- acceptance-tests/src/test/java/hudson/plugins/gradle/AbstractAcceptanceTest.java
+++ acceptance-tests/src/test/java/hudson/plugins/gradle/AbstractAcceptanceTest.java
@@ -1,10 +1,6 @@
 package hudson.plugins.gradle;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import hudson.plugin.gradle.EnvironmentVariablesSettings;
 import org.apache.commons.io.FileUtils;
@@ -16,33 +12,25 @@ import org.jenkinsci.test.acceptance.controller.JenkinsController;
 import org.jenkinsci.test.acceptance.controller.LocalController;
 import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
 import org.jenkinsci.test.acceptance.junit.Resource;
-import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.zeroturnaround.zip.ZipUtil;
-import ratpack.handling.Context;
-import ratpack.test.embed.EmbeddedApp;
 
 import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.net.URI;
 import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public abstract class AbstractAcceptanceTest extends AbstractJUnitTest {
 
-    public static final String PUBLIC_BUILD_SCAN_ID = "z7o6hj5ag6bpc";
-    public static final String DEFAULT_SCAN_UPLOAD_TOKEN = "scan-upload-token";
+    @Rule
+    public MockGeServer mockGeServer = new MockGeServer();
 
     @Inject
     public JenkinsController jenkinsController;
-
-    private ObjectWriter jsonWriter = new ObjectMapper(new JsonFactory()).writer();
-
-    private EmbeddedApp mockGeServer;
 
     @Before
     public void commonBeforeEach() throws IOException {
@@ -54,56 +42,6 @@ public abstract class AbstractAcceptanceTest extends AbstractJUnitTest {
             FileUtils.copyFileToDirectory(
                 resource("/hudson.tasks.Maven.MavenInstaller").asFile(), updatesDirectory);
         }
-
-        mockGeServer = createMockGeServer();
-    }
-
-    @After
-    public void commonAfterEach() {
-        mockGeServer.close();
-    }
-
-    private EmbeddedApp createMockGeServer() {
-        return EmbeddedApp.fromHandlers(c -> {
-            c.prefix("scans/publish", a -> {
-                a
-                    .post("gradle/:pluginVersion/token", ctx -> {
-                        Map<String, String> body =
-                            ImmutableMap.of(
-                                "id", PUBLIC_BUILD_SCAN_ID,
-                                "scanUrl", publicBuildScanId(),
-                                "scanUploadUrl", scanUploadUrl(ctx),
-                                "scanUploadToken", DEFAULT_SCAN_UPLOAD_TOKEN
-                            );
-
-                        ctx.getResponse()
-                            .contentType("application/vnd.gradle.scan-ack+json")
-                            .send(jsonWriter.writeValueAsBytes(body));
-                    })
-                    .post("gradle/:pluginVersion/upload", ctx -> {
-                        ctx.getRequest().getBody(1024 * 1024 * 10)
-                            .then(__ ->
-                                ctx.getResponse()
-                                    .contentType("application/vnd.gradle.scan-upload-ack+json")
-                                    .send());
-                    })
-                    .notFound();
-            });
-        });
-    }
-
-    private String scanUploadUrl(Context ctx) {
-        String pluginVersion = ctx.getPathTokens().get("pluginVersion");
-        return String.format("%sscans/publish/gradle/%s/upload", mockGeServerAddress(), pluginVersion);
-    }
-
-    protected final String publicBuildScanId() {
-        return String.format("%ss/%s", mockGeServerAddress(), PUBLIC_BUILD_SCAN_ID);
-    }
-
-    protected final URI mockGeServerAddress() {
-        Preconditions.checkNotNull(mockGeServer, "mockGeServer has not yet been created");
-        return mockGeServer.getAddress();
     }
 
     protected final void addGlobalEnvironmentVariables(String... variables) {
--- acceptance-tests/src/test/java/hudson/plugins/gradle/GradleInjectionTest.java
+++ acceptance-tests/src/test/java/hudson/plugins/gradle/GradleInjectionTest.java
@@ -1,8 +1,11 @@
 package hudson.plugins.gradle;
 
 import com.google.common.collect.ImmutableMap;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.text.StrSubstitutor;
 import org.apache.commons.lang3.SystemUtils;
+import org.hamcrest.Matcher;
+import org.jenkinsci.test.acceptance.junit.Resource;
 import org.jenkinsci.test.acceptance.junit.WithPlugins;
 import org.jenkinsci.test.acceptance.plugins.gradle.GradleInstallation;
 import org.jenkinsci.test.acceptance.plugins.gradle.GradleStep;
@@ -10,16 +13,28 @@ import org.jenkinsci.test.acceptance.po.Build;
 import org.jenkinsci.test.acceptance.po.FreeStyleJob;
 import org.jenkinsci.test.acceptance.po.WorkflowJob;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.jenkinsci.test.acceptance.Matchers.containsString;
 import static org.junit.Assume.assumeTrue;
 
 @WithPlugins("gradle")
 public class GradleInjectionTest extends AbstractAcceptanceTest {
 
+    private static final String AGENT_VERSION = "3.11.1";
     private static final String GRADLE_VERSION = "Gradle 7.5.1";
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
 
     @Before
     public void beforeEach() {
@@ -27,8 +42,8 @@ public class GradleInjectionTest extends AbstractAcceptanceTest {
 
         addGlobalEnvironmentVariables(
             "JENKINSGRADLEPLUGIN_GRADLE_ENTERPRISE_INJECTION", "true",
-            "JENKINSGRADLEPLUGIN_GRADLE_ENTERPRISE_PLUGIN_VERSION", "3.11.1",
-            "JENKINSGRADLEPLUGIN_GRADLE_ENTERPRISE_URL", mockGeServerAddress().toString()
+            "JENKINSGRADLEPLUGIN_GRADLE_ENTERPRISE_PLUGIN_VERSION", AGENT_VERSION,
+            "JENKINSGRADLEPLUGIN_GRADLE_ENTERPRISE_URL", mockGeServer.getAddress().toString()
         );
     }
 
@@ -50,6 +65,10 @@ public class GradleInjectionTest extends AbstractAcceptanceTest {
         // then
         build.shouldSucceed();
         assertBuildScanPublished(build);
+
+        MockGeServer.ScanTokenRequest scanTokenRequest = mockGeServer.getLastScanTokenRequest();
+        assertThat(scanTokenRequest, notNullValue());
+        assertThat(scanTokenRequest.agentVersion, is(equalTo(AGENT_VERSION)));
 
         // TODO: Check that build scan is visible on the page
     }
@@ -82,14 +101,72 @@ public class GradleInjectionTest extends AbstractAcceptanceTest {
         // then
         build.shouldSucceed();
         assertBuildScanPublished(build);
+
+        MockGeServer.ScanTokenRequest scanTokenRequest = mockGeServer.getLastScanTokenRequest();
+        assertThat(scanTokenRequest, notNullValue());
+        assertThat(scanTokenRequest.agentVersion, is(equalTo(AGENT_VERSION)));
+    }
+
+    @Test
+    public void skipInjectionIfPluginAlreadyApplied() {
+        // given
+        String expectedAgentVersion = "3.10.3";
+
+        FreeStyleJob job = jenkins.jobs.create(FreeStyleJob.class);
+
+        job.copyResource(resource("/simple_gradle_project/build.gradle"), "build.gradle");
+        job.copyResource(settingsWithGradleEnterprise(expectedAgentVersion), "settings.gradle");
+        GradleStep gradle = job.addBuildStep(GradleStep.class);
+        gradle.setVersion(GRADLE_VERSION);
+        gradle.setSwitches("--no-daemon");
+        gradle.setTasks("helloWorld");
+        job.save();
+
+        // when
+        Build build = job.startBuild();
+
+        // then
+        build.shouldSucceed();
+        assertBuildScanPublished(build, false);
+
+        MockGeServer.ScanTokenRequest scanTokenRequest = mockGeServer.getLastScanTokenRequest();
+        assertThat(scanTokenRequest, notNullValue());
+        assertThat(scanTokenRequest.agentVersion, is(equalTo(expectedAgentVersion)));
     }
 
     private void assertBuildScanPublished(Build build) {
+        assertBuildScanPublished(build, true);
+    }
+
+    private void assertBuildScanPublished(Build build, boolean requireAppliedViaInitScript) {
         String output = build.getConsole();
 
-        assertThat(output, containsString("Applying com.gradle.enterprise.gradleplugin.GradleEnterprisePlugin via init script"));
+        Matcher<String> appliedViaInitScriptMatcher =
+            containsString("Applying com.gradle.enterprise.gradleplugin.GradleEnterprisePlugin via init script");
+        assertThat(output, requireAppliedViaInitScript ? appliedViaInitScriptMatcher : not(appliedViaInitScriptMatcher));
+
         assertThat(output, containsString("> Task :helloWorld"));
         assertThat(output, containsString("Hello, World!"));
-        assertThat(output, containsString("Publishing build scan..." + System.lineSeparator() + publicBuildScanId()));
+        assertThat(output, containsString("Publishing build scan..." + System.lineSeparator() + mockGeServer.publicBuildScanId()));
+    }
+
+    private Resource settingsWithGradleEnterprise(String gePluginVersion) {
+        String settingsTemplate = resource("/settings_with_ge.gradle.template").asText();
+        String settings =
+            new StrSubstitutor(
+                ImmutableMap.of(
+                    "ge_plugin_version", gePluginVersion,
+                    "server", mockGeServer.getAddress()
+                ))
+                .replace(settingsTemplate);
+
+        try {
+            File tmp = tempFolder.newFile();
+            FileUtils.writeStringToFile(tmp, settings, StandardCharsets.UTF_8);
+
+            return new Resource(tmp.toURI().toURL());
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 }
