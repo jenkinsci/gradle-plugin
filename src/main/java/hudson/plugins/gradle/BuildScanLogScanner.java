9a1278732d3a2e7e6edc09053600e4ec901545cc
Sorin Florea <sflorea@gradle.com>
2022-11-10 08:40:11 +0100

    Improve build scan link capture

--- src/main/java/hudson/plugins/gradle/BuildScanLogScanner.java
+++ src/main/java/hudson/plugins/gradle/BuildScanLogScanner.java
@@ -1,31 +1,37 @@
 package hudson.plugins.gradle;
 
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BuildScanLogScanner {
+    private static final Pattern BUILD_SCAN_PATTERN = Pattern.compile("Publishing (build scan|build information)\\.\\.\\.");
+    private static final Pattern URL_PATTERN = Pattern.compile(".*(?:\\[INFO] )?(https?://.*/s/.*)");
+    private static final int LINES_TO_SCAN = 1000;
     private final BuildScanPublishedListener listener;
     private int linesSinceBuildScanPublishingMessage = Integer.MAX_VALUE;
-    private static final Pattern BUILD_SCAN_PATTERN = Pattern.compile("Publishing (build scan|build information)\\.\\.\\.");
-    private static final Pattern URL_PATTERN = Pattern.compile("https?://\\S*");
 
     public BuildScanLogScanner(BuildScanPublishedListener listener) {
         this.listener = listener;
     }
 
     void scanLine(String line) {
-        if (linesSinceBuildScanPublishingMessage < 10) {
+        if (linesSinceBuildScanPublishingMessage < LINES_TO_SCAN) {
             linesSinceBuildScanPublishingMessage++;
-            Matcher matcher = URL_PATTERN.matcher(line);
-            if (matcher.find()) {
+            Optional<String> maybeBuildScanUrl = doGetBuildScanId(line);
+            if (maybeBuildScanUrl.isPresent()) {
                 linesSinceBuildScanPublishingMessage = Integer.MAX_VALUE;
-                String buildScanUrl = matcher.group();
-                listener.onBuildScanPublished(buildScanUrl);
+                listener.onBuildScanPublished(maybeBuildScanUrl.get());
             }
         }
         if (BUILD_SCAN_PATTERN.matcher(line).find()) {
             linesSinceBuildScanPublishingMessage = 0;
         }
 
+    }
+
+    private static Optional<String> doGetBuildScanId(String text) {
+        Matcher matcher = URL_PATTERN.matcher(text);
+        return matcher.matches() ? Optional.of(matcher.group(1)) : Optional.empty();
     }
 }
---
+++ src/test/groovy/hudson/plugins/gradle/BuildScanLogScannerTest.groovy
@@ -0,0 +1,53 @@
+package hudson.plugins.gradle
+
+
+import spock.lang.Specification
+import spock.lang.Subject
+import spock.lang.Unroll
+
+@Unroll
+@Subject(BuildScanLogScanner)
+class BuildScanLogScannerTest extends Specification {
+
+    def 'properly captures build scan ur given #log'(List<String> log, List<String> expectedUrls) {
+        given:
+        def listener = new MockBuildScanPublishedListener();
+        def scanner = new BuildScanLogScanner(listener)
+
+        when:
+        log.each { scanner.scanLine(it) }
+
+        then:
+        listener.urls == expectedUrls
+
+        where:
+        log                                                                                                         || expectedUrls
+        logWithBuildScans(["https://scans.gradle.com/s/bzb4vn64kx3bc"])                                             || ["https://scans.gradle.com/s/bzb4vn64kx3bc"]
+        logWithBuildScans(["https://scans.gradle.com/s/bzb4vn64kx3bc", "https://scans.gradle.com/s/asc9wm73ly1do"]) || ["https://scans.gradle.com/s/bzb4vn64kx3bc", "https://scans.gradle.com/s/asc9wm73ly1do"]
+        logWithBuildScans(["https://scans.gradle.com/bzb4vn64kx3bc"])                                               || []
+        logWithBuildScans(["https://scans.gradle.com/bzb4vn64kx3bc"], 1010)                                         || []
+    }
+
+    static List<String> logWithBuildScans(List<String> scanLinks, linesBetween = 10) {
+        def log = new ArrayList<String>()
+
+        scanLinks.forEach { url ->
+            log.addAll(["Some log", "other log", "https://gradle.com/s/1234567890", "more log", "and more log"])
+            log.add("Publishing build scan...")
+            1..linesBetween.each { log.add("Some in-between log") }
+            log.add(url)
+            log.addAll(["Some log", "other log"])
+        }
+
+        log
+    }
+
+    class MockBuildScanPublishedListener implements BuildScanPublishedListener {
+        List<String> urls = []
+
+        @Override
+        void onBuildScanPublished(String scanUrl) {
+            urls.add(scanUrl)
+        }
+    }
+}
