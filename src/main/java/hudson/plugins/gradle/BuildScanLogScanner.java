873f1399253f19cb5d9a2c0e41f53c47ed2ef151
Alexey Venderov <avenderov@gradle.com>
2023-05-05 15:04:03 +0200

    Do not create Optional instance

--- src/main/java/hudson/plugins/gradle/BuildScanLogScanner.java
+++ src/main/java/hudson/plugins/gradle/BuildScanLogScanner.java
@@ -1,14 +1,17 @@
 package hudson.plugins.gradle;
 
-import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BuildScanLogScanner {
+
     private static final Pattern BUILD_SCAN_PATTERN = Pattern.compile("Publishing (build scan|build information)\\.\\.\\.");
     private static final Pattern URL_PATTERN = Pattern.compile(".*(?:\\[INFO] )?(https?://.*/s/.*)");
     private static final int LINES_TO_SCAN = 1000;
+
     private final BuildScanPublishedListener listener;
+
     private int linesSinceBuildScanPublishingMessage = Integer.MAX_VALUE;
 
     public BuildScanLogScanner(BuildScanPublishedListener listener) {
@@ -18,7 +21,7 @@ public class BuildScanLogScanner {
     void scanLine(String line) {
         if (linesSinceBuildScanPublishingMessage < LINES_TO_SCAN) {
             linesSinceBuildScanPublishingMessage++;
-            tryFindBuildScanUrl(line).ifPresent(url -> {
+            tryFindBuildScanUrl(line, url -> {
                 linesSinceBuildScanPublishingMessage = Integer.MAX_VALUE;
                 listener.onBuildScanPublished(url);
             });
@@ -29,8 +32,10 @@ public class BuildScanLogScanner {
 
     }
 
-    private static Optional<String> tryFindBuildScanUrl(String text) {
+    private static void tryFindBuildScanUrl(String text, Consumer<String> action) {
         Matcher matcher = URL_PATTERN.matcher(text);
-        return matcher.matches() ? Optional.of(matcher.group(1)) : Optional.empty();
+        if (matcher.matches()) {
+            action.accept(matcher.group(1));
+        }
     }
 }
