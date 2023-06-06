85a7910b19a18041517f44137fae1dc49b47f8e5
Alexis Tual <atual@gradle.com>
2023-06-06 14:28:27 +0200

    Bump CCUD Maven extension to 1.12

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ val ciJenkinsBuild by gradleExt { System.getenv("JENKINS_URL") != null }
 val ciTeamCityBuild by gradleExt { System.getenv("TEAMCITY_VERSION") != null }
 val isCi by gradleExt { ciJenkinsBuild || ciTeamCityBuild }
 val gradleEnterpriseMavenExtensionVersion by gradleExt { "1.17.2" }
-val commonCustomUserDataMavenExtensionVersion by gradleExt { "1.11.1" }
+val commonCustomUserDataMavenExtensionVersion by gradleExt { "1.12" }
 
 gradleEnterprise {
     buildScan {
