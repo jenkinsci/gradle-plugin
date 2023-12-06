73e5e009bd0170e40f760cce77177057c02a2da9
Inaki Villar <inaki.seri@gmail.com>
2023-12-06 17:45:12 +0100

    update ccud maven 1.12.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ val ciJenkinsBuild by gradleExt { System.getenv("JENKINS_URL") != null }
 val ciTeamCityBuild by gradleExt { System.getenv("TEAMCITY_VERSION") != null }
 val isCi by gradleExt { ciJenkinsBuild || ciTeamCityBuild }
 val gradleEnterpriseMavenExtensionVersion by gradleExt { "1.20" }
-val commonCustomUserDataMavenExtensionVersion by gradleExt { "1.12.4" }
+val commonCustomUserDataMavenExtensionVersion by gradleExt { "1.12.5" }
 
 gradleEnterprise {
     projectId = "jenkinsci-gradle-plugin"
