fcc42137ffae7d2cf323d26dced9c9dcf1893c3a
daz <daz@gradle.com>
2025-04-05 09:01:55 -0600

    Adapt for renamed init-script parameters

--- src/main/java/hudson/plugins/gradle/injection/InitScriptVariables.java
+++ src/main/java/hudson/plugins/gradle/injection/InitScriptVariables.java
@@ -1,21 +1,22 @@
 package hudson.plugins.gradle.injection;
 
 public enum InitScriptVariables {
-    DEVELOCITY_AUTO_INJECTION_CUSTOM_VALUE("develocity.auto-injection.custom-value"),
-    DEVELOCITY_INIT_SCRIPT_NAME("develocity.injection.init-script-name"),
-    DEVELOCITY_INJECTION_ENABLED("develocity.injection-enabled"),
-    GRADLE_PLUGIN_REPOSITORY_URL("gradle.plugin-repository.url"),
-    GRADLE_PLUGIN_REPOSITORY_USERNAME("gradle.plugin-repository.username"),
-    GRADLE_PLUGIN_REPOSITORY_PASSWORD("gradle.plugin-repository.password"),
-    DEVELOCITY_PLUGIN_VERSION("develocity.plugin.version"),
-    CCUD_PLUGIN_VERSION("develocity.ccud-plugin.version"),
-    DEVELOCITY_URL("develocity.url"),
-    DEVELOCITY_ENFORCE_URL("develocity.enforce-url"),
-    DEVELOCITY_ALLOW_UNTRUSTED_SERVER("develocity.allow-untrusted-server"),
-    DEVELOCITY_CAPTURE_FILE_FINGERPRINTS("develocity.capture-file-fingerprints"),
-    DEVELOCITY_BUILD_SCAN_UPLOAD_IN_BACKGROUND("develocity.build-scan.upload-in-background"),
-    DEVELOCITY_TERMS_OF_USE_URL("develocity.terms-of-use.url"),
-    DEVELOCITY_TERMS_OF_USE_AGREE("develocity.terms-of-use.agree");
+    DEVELOCITY_INJECTION_CUSTOM_VALUE("develocity-injection.custom-value"),
+    DEVELOCITY_INIT_SCRIPT_NAME("develocity-injection.init-script-name"),
+    DEVELOCITY_INJECTION_ENABLED("develocity-injection.enabled"),
+    DEVELOCITY_INJECTION_DEBUG("develocity-injection.debug"),
+    GRADLE_PLUGIN_REPOSITORY_URL("develocity-injection.plugin-repository.url"),
+    GRADLE_PLUGIN_REPOSITORY_USERNAME("develocity-injection.plugin-repository.username"),
+    GRADLE_PLUGIN_REPOSITORY_PASSWORD("develocity-injection.plugin-repository.password"),
+    DEVELOCITY_PLUGIN_VERSION("develocity-injection.develocity-plugin.version"),
+    CCUD_PLUGIN_VERSION("develocity-injection.ccud-plugin.version"),
+    DEVELOCITY_URL("develocity-injection.url"),
+    DEVELOCITY_ENFORCE_URL("develocity-injection.enforce-url"),
+    DEVELOCITY_ALLOW_UNTRUSTED_SERVER("develocity-injection.allow-untrusted-server"),
+    DEVELOCITY_CAPTURE_FILE_FINGERPRINTS("develocity-injection.capture-file-fingerprints"),
+    DEVELOCITY_UPLOAD_IN_BACKGROUND("develocity-injection.upload-in-background"),
+    DEVELOCITY_TERMS_OF_USE_URL("develocity-injection.terms-of-use.url"),
+    DEVELOCITY_TERMS_OF_USE_AGREE("develocity-injection.terms-of-use.agree");
 
     private final String templateName;
 
