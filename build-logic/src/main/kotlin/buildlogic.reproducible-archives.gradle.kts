2c7974aada99815305b53af39dfeaee0d0ff0a3c
Alexis Tual <atual@gradle.com>
2025-11-07 11:22:29 -0600

    Remove Gradle files and update default Jenkins version in acceptance-tests (#686)

--- build-logic/src/main/kotlin/buildlogic.reproducible-archives.gradle.kts
+++
@@ -1,5 +0,0 @@
-// See https://docs.gradle.org/current/userguide/working_with_files.html#sec:reproducible_archives
-tasks.withType<AbstractArchiveTask>().configureEach {
-    isPreserveFileTimestamps = false
-    isReproducibleFileOrder = true
-}
